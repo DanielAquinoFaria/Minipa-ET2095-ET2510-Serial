521e442a4719598c0e9341a7a569334d771a952e
DanielAquinoFaria <127145718+DanielAquinoFaria@users.noreply.github.com>
2023-03-21 13:21:03 -0300

    Update ET2510.py

--- ET2510.py
+++ ET2510.py
@@ -9,8 +9,8 @@ class ET2510:
         while len(seq_hex) < 11:
             out = self.ser.read()
             seq_hex.append(out.hex())
-        seq_hex.insert(0, seq_hex.pop(
-            len(seq_hex) - 1))  # reorganiza a lista movendo todos os itens uma posição para direita
+        # reorganiza a lista movendo todos os itens uma posição para direita
+        seq_hex.insert(0, seq_hex.pop(len(seq_hex) - 1))
 
         self.seq_hex = seq_hex
         self.ser.close()
@@ -28,10 +28,40 @@ class ET2510:
         self.unidades = {"3b": 0, "b3": 1, "31": 2, "3d": 3, "b6": 4, "32": 5, "b0": 6}
         self.tablei = None
 
+    def acdc(self):  # ac dc
+        if self.seq_hex[9] == 'ba' or self.seq_hex[9] == '38':
+            signal = 'DC'
+        elif self.seq_hex[9] == 'b6' or self.seq_hex[9] == '34':
+            signal = 'AC'
+        else:
+            signal = ' '
+        return signal
+
     def chama_tabela(self, a):
+        # função para coletar itens na lista tabela, onde se fixa o slot numero 6 do seq_hex
         self.tablei = a
-        valor = self.table[self.unidades.get(self.seq_hex[6])][self.virgula()][a]
+        virgula = int(self.seq_hex[1][1])
+        valor = self.table[self.unidades.get(self.seq_hex[6])][virgula][a]
         return valor
+
+    def display(self):
+        display = [self.seq_hex[i][1] for i in range(2, 6)]  # coleta os 4 caracteres dos display
+
+        # função para coleta da unidade e grandeza
+        multiplo = self.chama_tabela(0)
+        display_info = int(''.join(display))
+
+        if display_info == 6000:  # check para overload
+            medida = 'OL'
+        elif self.seq_hex[6] == '31':
+            if display_info >= 2758:
+                medida = 'OL'
+            else:
+                medida = round(display_info * multiplo, len(str(multiplo)) - 1)
+        else:
+            medida = round(display_info * multiplo, len(str(multiplo)) + 2)
+
+        return medida
 
     def erro(self):
         """
@@ -40,78 +70,38 @@ class ET2510:
         .erro[1]: erro percentual
         .erro[2]: erro digital
         """
-        # if self.seq_hex[6] == '31':
-        err = self.chama_tabela(2)
         if self.display() == 'OL':
-            erroPer = ''
-            erroDig = ''
-            erroTotal = ''
+            return '', '', ''
+        if self.seq_hex[9] == 'b6' or self.seq_hex[9] == '34':
+            err = self.chama_tabela(4)
+            digi = self.chama_tabela(5)
         else:
-            if self.seq_hex[6] == '31':
-                erroPer = str(round((float(self.display()) * err / 100), len(str(self.chama_tabela(0)))-3))
-                digi = self.chama_tabela(3)
-                erroDig = digi * self.chama_tabela(0)*10
-            else:
-                erroPer = str(round((float(self.display()) * err / 100), len(str(self.chama_tabela(0)))-2))
-                digi = self.chama_tabela(3)
-                erroDig = digi * self.chama_tabela(0)
-            erroTotal = str(float(erroPer) + float(erroDig))[0:len(str(self.chama_tabela(0)))]
+            err = self.chama_tabela(2)
+            digi = self.chama_tabela(3)
+        factor = 10 if self.seq_hex[6] == '31' else 1
+
+        erroPer = round(float(self.display()) * err / 100, len(str(self.chama_tabela(0))) - (3 if factor == 10 else 2))
+        erroDig = digi * self.chama_tabela(0) * factor
+        erroTotal = round(erroPer + erroDig, len(str(self.chama_tabela(0))))
 
         return erroTotal, erroPer, erroDig
 
-    def virgula(self):  # utilizado para referenciar unidade e grandeza na 'table'
-        virgula = int(self.seq_hex[1][1])
-        return virgula
-
-    def display(self):
-        display = []
-        # função de coleta dos algarismos da medida
-        for i in range(2, 6):  # coleta os 4 caracteres dos display
-            displayTemp = self.seq_hex[i]
-            display.append(displayTemp[1])  # adiciona a lista o segundo termo do ‘item’ i de seq_hex
-
-        # função para coleta da unidade e grandeza
-        multiplo = self.chama_tabela(0)
-        display_info = (int(display[0]) * 1000 + int(display[1]) * 100 + int(display[2]) * 10 + int(display[3]))
-
-        if display_info == 6000:  # check para overload
-            medida = 'OL'
-        elif self.seq_hex[6] == '31':
-            if display_info >= 2758:
-                medida = 'OL'
-            else:
-                medida = str(display_info * multiplo)[0:len(str(self.chama_tabela(0)))-1]
-        else:
-            medida = str(display_info * multiplo)[0:len(str(self.chama_tabela(0)))+2]
-        return medida
-
-    def unidade(self):  # unidade de medida
-        unidade = self.chama_tabela(1)
-
-        return unidade
-
     def polaridade(self):
-        polaridade = []
-        # bloco leitura polaridade
         if self.seq_hex[7] == '38':
             polaridade = '+'
-        if self.seq_hex[7] == 'bc':
+        elif self.seq_hex[7] == 'bc':
             polaridade = '-'
-        if self.seq_hex[7] == 'b9':
+        else:
             polaridade = ''
         return polaridade
 
-    def acdc(self):  # ac dc
-        if self.seq_hex[9] == 'ba' or '38':
-            signal = 'DC'
-        elif self.seq_hex[9] == 'b6' or '34':
-            signal = 'AC'
-        else:
-            signal = ' '
-        return signal
+    def unidade(self):  # unidade de medida
+        unidade = self.chama_tabela(1)
+        return unidade
 
 
 if __name__ == '__main__':
+
     while True:
         med = ET2510('COM1')
         print(med.polaridade(), med.display(), " ", med.unidade(), " ", med.acdc(), " ", med.erro()[0], " ",
